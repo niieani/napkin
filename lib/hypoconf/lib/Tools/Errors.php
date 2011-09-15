4723a1cfe1a08cc573a700724f3c89a08121b066
Bazyli Brzóska <creator@amplifuge.com>
2011-09-15 15:11:19 +0200

    cleanup and refactoring

---
+++ lib/hypoconf/lib/Tools/Errors.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: NIXin
+ * Date: 15.09.2011
+ * Time: 14:26
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class Errors {
+
+}
